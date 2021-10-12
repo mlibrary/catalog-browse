43857cff00b9172501696baafc7cad07ebc913d4
Jon Earley <Jonathan.a.earley@gmail.com>
2021-10-12 15:48:23 -0400

    Mock the fields

--- slim-demo.rb
+++ slim-demo.rb
@@ -2,5 +2,24 @@ require "sinatra"
 require "slim"
 
 get "/" do
-  slim :browse
+  fields = [
+    {
+      label: "Keyword",
+      value: "keyword"
+    },
+    {
+      label: "Author",
+      value: "author"
+    },
+    {
+      label: "Title",
+      value: "title"
+    },
+    {
+      label: "Browse by (LC) call number",
+      value: "browse-by-callnumber"
+    }
+  ]
+
+  slim :browse, :locals => { :fields => fields}
 end
