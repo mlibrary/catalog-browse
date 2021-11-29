d050742beb6b3c0de433c11d0e8bb825fa7fedfd
Monique Rio <mrio@umich.edu>
2021-11-29 14:32:16 -0500

    added tests for browse_item; added code for mocking calls to solr

--- lib/models/browse_item.rb
+++ lib/models/browse_item.rb
@@ -6,15 +6,21 @@ class BrowseItem
     @catalog_doc = catalog_doc || {}
     @index_doc = index_doc
   end
-  #for the view
-  def title
-    [bib_title, author].join(" ") 
+  def match_notice?
+    false
   end
+  #for the view
   def url
     "https://search.lib.umich.edu/catalog/record/#{mms_id}"
   end
+  def title
+    [bib_title, author].join(" ") 
+  end
   def alternate_title
-    [alternate_bib_title, alternate_author].join(" ")
+    vernacular_title
+  end
+  def vernacular_title
+    [vernacular_bib_title, vernacular_author].join(" ")
   end
   def callnumber
     @index_doc["callnumber"]&.strip
@@ -24,6 +30,7 @@ class BrowseItem
   end
   
 
+  private
   #component pieces
   def mms_id
     @index_doc["bib_id"]
@@ -31,19 +38,16 @@ class BrowseItem
   def bib_title
     @catalog_doc["title"]&.first
   end
-  def alternate_bib_title
+  def vernacular_bib_title
     @catalog_doc["title"]&.slice(1)
   end
   def author
     @catalog_doc["mainauthor"]&.first
   end
-  def alternate_author
+  def vernacular_author
     @catalog_doc["mainauthor"]&.slice(1)
   end
   def publisher
     @catalog_doc["publisher"]&.first
-  end
-  def match_notice?
-    false
   end
 end
---
+++ spec/models/browse_item_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../spec_helper.rb'
+describe BrowseItem do
+  before(:each) do
+    @catalog_doc = JSON.parse(fixture('zhizn_bib.json'))
+    @index_doc = JSON.parse(fixture('zhizn_browse.json'))
+  end
+  subject do
+    described_class.new(@catalog_doc, @index_doc)
+  end
+  it "has false match_notice?" do
+    expect(subject.match_notice?).to eq(false)
+  end
+  it "shows appropriate url" do
+    expect(subject.url).to eq("https://search.lib.umich.edu/catalog/record/990039902820106381")
+  end
+  it "shows appropriate title" do
+    expect(subject.title).to eq("Zhiznʹ gospodina de Molʹera / Bulgakov, Mikhail, 1891-1940.")
+  end
+  it "shows appropriate vernacular title" do
+    expect(subject.vernacular_title).to eq("Жизнь господина де Мольера / Булгаков, Михаил, 1891-1940.")
+  end
+  it "shows callnumber" do
+    expect(subject.callnumber).to eq("PQ 1852 .B85 1992")
+  end
+  it "shows subtitles" do
+    expect(subject.subtitles).to eq(["Bulgakov, Mikhail, 1891-1940.","\"Delovoĭ t͡sentr\","])
+  end
+end
