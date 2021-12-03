56717a3c905e2989741f169819308ac937839141
Monique Rio <mrio@umich.edu>
2021-12-03 11:11:32 -0500

    hotfix for empty query

--- catalog-browse.rb
+++ catalog-browse.rb
@@ -12,9 +12,12 @@ get '/callnumber' do
   datastores = YAML.load_file("./config/datastores.yml") 
   
   callnumber = params[:query]
-  redirect "/" if callnumber.nil?
   reference_id = params[:reference_id] || callnumber 
-  list = BrowseList.for(direction: params[:direction], reference_id: reference_id, num_rows_to_display: 20, original_reference: callnumber, banner_reference: params[:banner_reference])
+  if callnumber.nil?
+    list = OpenStruct.new(original_reference: '')
+  else
+    list = BrowseList.for(direction: params[:direction], reference_id: reference_id, num_rows_to_display: 20, original_reference: callnumber, banner_reference: params[:banner_reference])
+  end
   erb :layout, :locals  => {
     :fields => fields,
     :datastores => datastores,
