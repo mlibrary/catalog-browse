7a3a95a7493e4d1e6573031ca6ecfd1ea7f91e6f
Monique Rio <mrio@umich.edu>
2021-12-09 17:00:33 -0500

    added functions for vernacular fields to browse_item

--- lib/models/browse_item.rb
+++ lib/models/browse_item.rb
@@ -15,29 +15,11 @@ class BrowseItem
     "https://search.lib.umich.edu/catalog/record/#{mms_id}"
   end
   def title
-    [bib_title, author].join(" ") 
+    [@catalog_doc["title_display"]&.first, edition].compact.join(" ")
   end
   def vernacular_title
-    [vernacular_bib_title, vernacular_author].join(" ")
-  end
-  def callnumber
-    @index_doc["callnumber"]&.strip
-  end
-  def subtitles
-    [ author, publisher].compact
-  end
-  
-
-  private
-  #component pieces
-  def mms_id
-    @index_doc["bib_id"]
-  end
-  def bib_title
-    @catalog_doc["title_display"]&.first
-  end
-  def vernacular_bib_title
-    @catalog_doc["title_display"]&.slice(1)
+    output = @catalog_doc["title_display"]&.slice(1)
+    [output, edition].compact.join(" ") unless output.nil?
   end
   def author
     @catalog_doc["mainauthor"]&.first
@@ -45,7 +27,36 @@ class BrowseItem
   def vernacular_author
     @catalog_doc["mainauthor"]&.slice(1)
   end
+  def callnumber
+    @index_doc["callnumber"]&.strip
+  end
+  def subtitles
+    [ author, publisher].compact
+  end
   def publisher
     @catalog_doc["publisher"]&.first
   end
+  def vernacular_publisher
+    @catalog_doc["publisher"]&.slice(1)
+  end
+  
+
+  private
+  #component pieces
+  def edition
+    @catalog_doc["edition"]&.first
+  end
+  def mms_id
+    @index_doc["bib_id"]
+  end
+  def bib_title
+    @catalog_doc["title_display"]&.first
+  end
+  def bib_edition
+    @catalog_doc["edition"]
+  end
+  def vernacular_bib_title
+    @catalog_doc["title_display"]&.slice(1)
+  end
+  #publish brief?
 end
--- spec/models/browse_item_spec.rb
+++ spec/models/browse_item_spec.rb
@@ -14,11 +14,35 @@ describe BrowseItem do
   it "shows appropriate url" do
     expect(subject.url).to eq("https://search.lib.umich.edu/catalog/record/990039902820106381")
   end
-  it "shows appropriate title" do
-    expect(subject.title).to eq("Zhiznʹ gospodina de Molʹera / M. Bulgakov ; [podgot. teksta i poslesl. V.I. Loseva]. Bulgakov, Mikhail, 1891-1940.")
+  context "#title" do
+    it "shows appropriate title without edition when there isn't one" do
+      expect(subject.title).to eq("Zhiznʹ gospodina de Molʹera / M. Bulgakov ; [podgot. teksta i poslesl. V.I. Loseva].")
+    end
+    it "shows title with edition when there is one" do
+      @catalog_doc["edition"] = ["my edition"]
+      expect(subject.title).to eq("Zhiznʹ gospodina de Molʹera / M. Bulgakov ; [podgot. teksta i poslesl. V.I. Loseva]. my edition")
+    end
   end
-  it "shows appropriate vernacular title" do
-    expect(subject.vernacular_title).to eq("Жизнь господина де Мольера / М. Булгаков ; [подгот. текста и послесл. В.И. Лосева]. Булгаков, Михаил, 1891-1940.")
+  context "#vernacular_title"  do
+    it "shows vernacular title without edition when there isn't one" do
+      expect(subject.vernacular_title).to eq("Жизнь господина де Мольера / М. Булгаков ; [подгот. текста и послесл. В.И. Лосева].")
+    end
+    it "shows vernacular title with edition when there is one" do
+      @catalog_doc["edition"] = ["my edition"]
+      expect(subject.vernacular_title).to eq("Жизнь господина де Мольера / М. Булгаков ; [подгот. текста и послесл. В.И. Лосева]. my edition")
+    end
+  end
+  it "shows appropriate author" do
+    expect(subject.author).to eq("Bulgakov, Mikhail, 1891-1940.")
+  end
+  it "shows appropriate vernacular author" do
+    expect(subject.vernacular_author).to eq("Булгаков, Михаил, 1891-1940.")
+  end
+  it "shows appropriate publisher" do
+    expect(subject.publisher).to eq("\"Delovoĭ t͡sentr\",")
+  end
+  it "shows appropriate vernacular publisher" do
+    expect(subject.vernacular_publisher).to eq("\"Деловой центр\",")
   end
   it "shows callnumber" do
     expect(subject.callnumber).to eq("PQ 1852 .B85 1992")
