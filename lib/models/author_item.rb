59826155373fc9975518b2e03eab0d640363b58d
Monique Rio <mrio@umich.edu>
2022-07-27 11:26:19 -0400

    adds cross references and header links

--- lib/models/author_item.rb
+++ lib/models/author_item.rb
@@ -1,4 +1,11 @@
 class AuthorItem
+  def self.for(browse_doc:, exact_match:)
+    if browse_doc["record_type"] == "redirect"
+      AuthorItemWithCrossReferences.new(browse_doc: browse_doc, exact_match: exact_match)
+    else
+      AuthorItem.new(browse_doc: browse_doc, exact_match: exact_match)
+    end
+  end
   def initialize(browse_doc:, exact_match:)
     @browse_doc = browse_doc
     @exact_match = exact_match
@@ -43,6 +50,31 @@ class AuthorItem
   end
 
   def heading_link
-    @browse_doc["heading_link"]
+    @browse_doc["naf_id"]
   end
+end
+class AuthorItemWithCrossReferences < AuthorItem
+  def has_cross_references?
+    true
+  end
+  def cross_references
+    #see_instead because this still needs to be changed in solr
+    @browse_doc["see_instead"].map{|author| AuthorItemSee.new(author) }
+  end
+end
+class AuthorItemSee
+  attr_reader :author
+  def initialize(author)
+    @author = author&.strip
+  end
+  def kind
+    "see_also"
+  end
+  def url
+    "#{ENV.fetch("BASE_URL")}/author?query=#{URI.encode_www_form_component(@author)}"
+  end
+  def heading_link?
+    false
+  end
+  
 end
