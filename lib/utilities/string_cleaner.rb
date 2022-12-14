c3dc7c1b019bb86c5bc8daeab38363e07fd2270b
Monique Rio <mrio@umich.edu>
2022-12-14 09:07:17 -0500

    makes stringcleaner less intense

--- lib/utilities/string_cleaner.rb
+++ lib/utilities/string_cleaner.rb
@@ -2,22 +2,39 @@ module StringCleaner
   def self.prefixes
     # are there more of these?
     [
+      "academic_discipline",
       "author",
+      "call_number_starts_with",
+      "contains",
+      "contributor",
+      "date",
+      "isbn",
+      "isn",
+      "issn",
+      "journal_title",
+      "keyword",
+      "pmid",
+      "publication_date",
+      "publisher",
+      "realuth",
+      "series",
       "subject",
-      "academic_discipline"
+      "title",
+      "title_starts_with"
     ]
   end
 
   def self.strip_symbols(str)
-    str.gsub(/[\p{P}\p{Sm}\p{Sc}\p{So}^`]/, "")
+    # str.gsub(/[\p{P}\p{Sm}\p{Sc}\p{So}^`]/, "")
+    str.gsub(/["'(){}\[\]]/, "")
   end
 
   # TODO: add bits to remove field prefix (e.g., 'author:') as defined in 00-catalog.yml
   # this is where the author browse specific cleaning goes
   def self.cleanup_author_browse_string(str)
     prefixes.each do |x|
-      if str.match?(/^#{x}:"/)
-        str.sub!(/^#{x}:"/, "")
+      if str.match?(/^#{x}:["(]/)
+        str.sub!(/^#{x}:/, "")
         break
       end
     end
--- spec/utilities/string_cleaner_spec.rb
+++ spec/utilities/string_cleaner_spec.rb
@@ -1,31 +1,15 @@
 describe StringCleaner do
   context ".strip_symbols" do
-    it "removes punctuation" do
-      # for \p{P}
-      expect(described_class.strip_symbols('!"#%&\'()*,_./:;=?@[]\\_{}')).to eq("")
-    end
-    it "removes math symbols" do
-      # for \p{Sm}
-      expect(described_class.strip_symbols("+<=>|~≤≥±≠÷×")).to eq("")
-    end
-    it "removes currency symbols" do
-      # for \p{Sc}
-      expect(described_class.strip_symbols("$¢¥£€¤")).to eq("")
-    end
-    it "removes other symbols" do
-      # for \p{So}
-      expect(described_class.strip_symbols("©༕®°")).to eq("")
-    end
-    it "removes a few modifiers" do
-      expect(described_class.strip_symbols("`^")).to eq("")
+    it "removes certain punctuation" do
+      expect(described_class.strip_symbols('"\'(){}[]')).to eq("")
     end
   end
   context ".cleanup_author_browse_string" do
-    it "cleans up symbols" do
-      expect(described_class.cleanup_author_browse_string("©+$.")).to eq("")
-    end
     it "removes 'author:' prefix" do
       expect(described_class.cleanup_author_browse_string('author:"Author Name"')).to eq("Author Name")
+    end
+    it "removes 'isn(' prefix" do
+      expect(described_class.cleanup_author_browse_string('isn:("123-456")')).to eq("123-456")
     end
   end
 end
