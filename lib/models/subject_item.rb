24770b4236ad17f8722417753ca6f9dd4656259a
Monique Rio <mrio@umich.edu>
2023-02-15 11:55:04 -0500

    WIP adding subjects

---
+++ lib/models/subject_item.rb
@@ -0,0 +1,94 @@
+class SubjectItem
+  def self.for(browse_doc:, exact_match:)
+    if browse_doc.key?("see_also")
+      SubjectItemWithCrossReferences.new(browse_doc: browse_doc, exact_match: exact_match)
+    else
+      SubjectItem.new(browse_doc: browse_doc, exact_match: exact_match)
+    end
+  end
+
+  def initialize(browse_doc:, exact_match:)
+    @browse_doc = browse_doc
+    @exact_match = exact_match
+  end
+
+  def match_notice?
+    false
+  end
+
+  def exact_match?
+    !!@exact_match
+  end
+
+  def author
+    @browse_doc["term"]&.strip
+  end
+
+  def url
+    params = {library: "U-M Ann Arbor Libraries", query: "author:(\"#{author}\")", "filter.search_only": false}
+    "https://search.lib.umich.edu/catalog?#{URI.encode_www_form(params)}"
+  end
+
+  def results_count
+    @browse_doc["count"]
+  end
+
+  def record_text
+    "#{results_count} record#{"s" if results_count != 1}"
+  end
+
+  # mrio: commenting out because it isn't currently used in the UI
+  # def alternate_forms
+  # @browse_doc["alternate_forms"]
+  # end
+
+  def has_cross_references?
+    false
+  end
+
+  def heading_link?
+    !!heading_link
+  end
+
+  def heading_link
+    @browse_doc["loc_id"]
+  end
+end
+
+class SubjectItemWithCrossReferences < SubjectItem
+  def has_cross_references?
+    true
+  end
+
+  def cross_references
+    # see_instead because this still needs to be changed in solr
+    @browse_doc["see_also"].map { |author| SubjectItemSeeAlso.new(author) }
+  end
+end
+
+class SubjectItemSeeAlso
+  attr_reader :author, :count
+  def initialize(author)
+    @author, @count = author.split("||").map { |x| x.strip }
+  end
+
+  def author_display
+    "#{@author} (in author list)"
+  end
+
+  def record_text
+    "#{count} record#{"s" if count != 1}"
+  end
+
+  def kind
+    "see_also"
+  end
+
+  def url
+    "#{ENV.fetch("BASE_URL")}/author?query=#{URI.encode_www_form_component(@author)}"
+  end
+
+  def heading_link?
+    false
+  end
+end
---
+++ lib/models/subject_list.rb
@@ -0,0 +1,70 @@
+class SubjectList < BrowseListPresenter
+  def self.for(direction:, reference_id:, num_rows_to_display:, original_reference:, banner_reference:)
+    browse_list = BrowseList.for(
+      direction: direction,
+      reference_id: reference_id,
+      num_rows_to_display: num_rows_to_display,
+      original_reference: original_reference,
+      banner_reference: banner_reference,
+      browse_solr_client: BrowseSolrClient.new(core: ENV.fetch("AUTHORS_CORE"), match_field: "term", q: "browse_field:subject")
+    )
+
+    new(browse_list: browse_list)
+  end
+
+  def initialize(browse_list:)
+    @browse_list = browse_list
+  end
+
+  def feedback_url
+    # Subject Browse specific url
+    "https://umich.qualtrics.com/jfe/form/SV_43jm8oGIRVLEBbo"
+  end
+
+  def name
+    "subject"
+  end
+
+  def path
+    "subject"
+  end
+
+  def doc_title
+    "Browse by Subject"
+  end
+
+  def items
+    banner_index = nil
+    match_notice = OpenStruct.new(author: original_reference.upcase, match_notice?: true)
+    my_items = @browse_list.docs.map.with_index do |browse_doc, index|
+      exact_match = exact_matches.any?(browse_doc["id"])
+      banner_match = (banner_reference == browse_doc["id"])
+      banner_index = index if (exact_match || banner_match) && banner_index.nil?
+      SubjectItem.for(browse_doc: browse_doc, exact_match: exact_match)
+    end
+    banner_index.nil? ? my_items : my_items.insert(banner_index, match_notice)
+  end
+
+  def help_text
+    '<span class="strong">Browse by author help:</span> Search an author (last names, first name), organization or conference and view an alphabetical list of all author headings that link to matching records in the library catalog. Also view variations of some author names (pseudonyms) linked to that part of the author index.'
+  end
+end
+
+class SubjectList::Error < SubjectList
+  attr_reader :original_reference
+  def initialize(original_reference = "")
+    @original_reference = original_reference
+  end
+
+  def show_table?
+    false
+  end
+
+  def error?
+    true
+  end
+
+  def error_message
+    "<span class=\"strong\">#{original_reference}</span> is not a valid author query."
+  end
+end
